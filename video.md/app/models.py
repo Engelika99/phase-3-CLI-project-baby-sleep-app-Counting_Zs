6925b0fd857225133c6c531bf87b33b00a8f644e
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-08-26 09:31:41 -0400

    Creates parents table

--- video.md/app/models.py
+++ video.md/app/models.py
@@ -1,3 +1,13 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class Parent(Base):
+    __tablename__ = 'parents'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+    password_hash = Column(String)
