fa29a5f3922bfb56424dd5552dff72b693c4235e
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-08-27 23:17:59 -0400

    Update dependencies and create notes file

--- video.md/app/models.py
+++ video.md/app/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateT
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
 Base = declarative_base()
 
 class Parent(Base):
