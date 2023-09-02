74592a8a2636fb62cbd9e6f03204b0fc8a075602
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-09-01 23:59:52 -0400

    create new parent obj function to be saved to database

--- app/cli.py
+++ app/cli.py
@@ -12,3 +12,19 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 faker = Faker()
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+@click.option('--name', prompt='Enter Your Name', help='Please Enter Your User Name')
+@click.otion('--email', prompt='Enter Your Email', help='Please Enter Your Email')
+@click.option('--password', prompt='Enter Your Password', hide_password=True, successful_entry_prompt=True help='Please Enter Your Password')
+
+def create_parent(name, email, password_hash): 
+    parents = Parent(name=name, email=email, password_hash=password_hash)
+    session.add(parents)
+    session.commit()
+    click.echo(f"Parent '{name}' Added Successfully!")
+
