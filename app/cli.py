4e3b40a87543b74b6d3838f9b26e2c9e6442c16d
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-09-01 22:40:35 -0400

    create database interaction and imports

--- app/cli.py
+++ app/cli.py
@@ -1,4 +1,14 @@
 import click  
 from faker import Faker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Parent, Baby, BabySleepSchedule, BabySleepRecommendations
+from datetime import datetime
+
+# Create database interaction
+CountingZs_db = "sqlite:///baby_sleep_app_Counting_Zs.db"
+engine = create_engine(CountingZs_db)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 faker = Faker()
