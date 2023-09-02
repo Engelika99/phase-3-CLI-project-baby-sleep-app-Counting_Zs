6291b70c44ee9395ca179aa78e9e88b932fc1bc6
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-09-02 08:04:14 -0400

    1 to 15 babies can be saved for parent

--- app/seed.py
+++ app/seed.py
@@ -11,6 +11,7 @@ engine = create_engine(CountingZs_db)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+# create 20 parents
 for _ in range(20):
     parents = Parent(
         name=faker.name(),
@@ -22,3 +23,7 @@ for _ in range(20):
 
 session.commit()    
 
+#parent can have up to 15 babies saved(Teacher/daycare purpose)
+parents = session.query(Parent).all()
+for parents in parents:
+    for _ in range(1, 16):
